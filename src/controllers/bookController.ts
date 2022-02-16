0ab5d5312faf28c80649be3f250e132f98d07c23
JhonWilderParionaVilca <jhon.pariona@smartpyme.pe>
2022-02-16 18:47:22 -0500

    feat: complete test module 4

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -17,3 +17,34 @@ export const addNewBook = async (req: Request, res: Response) => {
     console.log(error);
   }
 };
+
+export const getBooks = async (req: Request, res: Response) => {
+  try {
+    const Book = BookModel;
+    const books: IBook[] = await Book.find({});
+    res.status(200).json({ books });
+  } catch (error) {
+    res.json({
+      msj: 'ocurrio un error',
+    });
+    console.log(error);
+  }
+};
+
+export const getBookById = async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.id;
+    console.log(bookId);
+    const Book = BookModel;
+    const book: IBook | null = await Book.findById(bookId);
+    if (!book) {
+      throw new Error('No existe el libro');
+    }
+    res.status(200).json({ book });
+  } catch (error) {
+    res.json({
+      msj: 'ocurrio un error',
+    });
+    console.log(error);
+  }
+};
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { addNewBook } from '../controllers/bookController';
+import {
+  addNewBook,
+  getBookById,
+  getBooks,
+} from '../controllers/bookController';
 
 const router = Router();
 
 router.post('/books', addNewBook);
+router.get('/books', getBooks);
+router.get('/books/:id', getBookById);
 
 export default router;
