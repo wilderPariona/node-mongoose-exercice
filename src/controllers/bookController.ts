44d0011705f18fa3b5dd44273d338cb1ccf22ac5
JhonWilderParionaVilca <jhon.pariona@smartpyme.pe>
2022-02-16 19:17:07 -0500

    feat: complete test module 5

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -34,13 +34,55 @@ export const getBooks = async (req: Request, res: Response) => {
 export const getBookById = async (req: Request, res: Response) => {
   try {
     const bookId = req.params.id;
-    console.log(bookId);
     const Book = BookModel;
     const book: IBook | null = await Book.findById(bookId);
     if (!book) {
       throw new Error('No existe el libro');
     }
     res.status(200).json({ book });
+  } catch (error) {
+    res.json({
+      msj: 'ocurrio un error',
+    });
+    console.log(error);
+  }
+};
+
+export const updateBook = async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.id;
+    const { title, overview, category, price, created_date } = req.body;
+    const newBookInfo = { title, overview, category, price, created_date };
+    const Book = BookModel;
+    const book: IBook | null = await Book.findOneAndUpdate({
+        _id: bookId,
+      },
+      newBookInfo,
+      {
+        new: true,
+      }
+    );
+    if (!book) {
+      throw new Error('No existe el libro');
+    }
+    res.status(200).json({ book });
+  } catch (error) {
+    res.json({
+      msj: 'ocurrio un error',
+    });
+    console.log(error);
+  }
+};
+
+export const deleteBook = async (req: Request, res: Response) => {
+  try {
+    const bookId = req.params.id;
+    const Book = BookModel;
+    await Book.deleteOne({
+      _id: bookId,
+    });
+
+    res.status(200).json({ message: 'book successfully deleted' });
   } catch (error) {
     res.json({
       msj: 'ocurrio un error',
--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import {
   addNewBook,
+  deleteBook,
   getBookById,
   getBooks,
+  updateBook,
 } from '../controllers/bookController';
 
 const router = Router();
@@ -10,5 +12,7 @@ const router = Router();
 router.post('/books', addNewBook);
 router.get('/books', getBooks);
 router.get('/books/:id', getBookById);
+router.put('/books/:id', updateBook);
+router.delete('/books/:id', deleteBook);
 
 export default router;
